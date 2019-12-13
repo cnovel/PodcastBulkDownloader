76a89928c48af6553c372caf4cc412a6d4f204c3
cnovel <github@kosmon.fr>
2019-12-13 19:10:34 +0100

    Two exe for the moment, one for CLI and one for GUI

--- create_exe.bat
+++ create_exe.bat
@@ -1,1 +1,3 @@
-pyinstaller.exe -w -F -i src\pbd_icon.ico src\app.py -n PodcastBulkDownloader
+pyinstaller.exe --clean -F src\app.py -n PodcastBulkDownloader -i src/pbd_icon.ico --noconsole
+pyinstaller.exe --clean -F src\bulk_downloader.py -n PodcastBulkDownloaderCLI -i src/pbd_icon.ico
+copy src\pbd_icon.ico dist\pbd_icon.ico /y
