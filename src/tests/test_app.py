89cda501111f15c5758577c8597ca0031eb5c80a
cnovel <github@kosmon.fr>
2022-06-20 21:55:28 +0200

    Switch to Windows

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -4,10 +4,8 @@ from src import app
 
 
 def test_app():
-    # Can't test if no display
-    if environ.get('DISPLAY', '') != '':
-        root = Tk()
-        a = app.PDBApp(root)
-        a.download()
-        a.fetch()
-        a.cancel()
+    root = Tk()
+    a = app.PDBApp(root)
+    a.download()
+    a.fetch()
+    a.cancel()
